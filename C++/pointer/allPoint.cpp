138ffa67c2bc6ba38ebfd764d06059b165150e56
Harshit_khanna <117803004+Harshittheprogramer69@users.noreply.github.com>
2026-01-19 21:07:15 +0530

    Pointers

---
+++ C++/pointer/allPoint.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 10;
+    int *y = &x;
+    int **z = &y;
+
+    cout << **z << endl;
+    cout << *z << endl;
+    cout << z << endl;
+    
+    return 0;
+}
