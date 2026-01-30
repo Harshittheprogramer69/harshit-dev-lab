3547498f4f487bfe24d9f93d1c79e79c82b76776
Harshit_khanna <117803004+Harshittheprogramer69@users.noreply.github.com>
2026-01-30 10:20:23 +0530

    CHANGES

--- C++/Classes_Objects/cars.cpp
+++ C++/Classes_Objects/cars.cpp
@@ -14,6 +14,9 @@ class CARS{
             total_cars = 0;
             this->company = company;
         }
+        void display() {
+            cout << "Total number of cars are " << total_cars << "in Company = " << company << endl;
+        }
         ~CARS() {
             cout << "Company fell apart" << endl;
         }
@@ -26,6 +29,11 @@ class TRUCK : public CARS {
         TRUCK(int total_cars, string company, string truck_name) : CARS(total_cars,company) {
             this->truck_name = truck_name;
         }
+        void showTruck() {
+            display();
+            cout << "Truck Name: " << truck_name << endl;
+        }
+
         ~TRUCK() {
             cout << "Trucks Crashed" << endl;
         }
