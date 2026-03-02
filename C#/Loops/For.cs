fd2951c2622cfbd241bab220631f2e068bccb2b3
Harshit_khanna <117803004+Harshittheprogramer69@users.noreply.github.com>
2026-03-02 14:39:46 +0530

    C#

---
+++ C#/Loops/For.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Loops
+{
+    class for_loop
+    {
+        static void Main()
+        {
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{i}. Running The Loop.");
+            }
+        }
+    }
+}
